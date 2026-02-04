963e1e6b69ba4ce61ec74d05603157c7933a4eea
jamesgotch <jimmygotch@icloud.com>
2026-02-04 15:49:17 +0000

    commit Feb4

--- Jan28/create.py
+++ Jan28/create.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 conn = sqlite3.connect('../baseball.db')
 cursor = conn.cursor()
-df = pd.read_csv('batting.csv')
-df.to_sql('batting', conn, if_exists= 'replace', index = False)
+df = pd.read_csv('../teams.csv')
+df.to_sql('teams', conn, if_exists='replace', index=False)
 conn.commit()
 conn.close()
